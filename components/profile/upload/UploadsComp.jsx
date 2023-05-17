3358905f6143cedbe53dbacbe4cd3ba7cbc961f4
Artem <bobrovniksuper@icloud.com>
2023-05-17 18:24:53 +0300

    some new things

--- components/profile/upload/UploadsComp.jsx
+++ components/profile/upload/UploadsComp.jsx
@@ -1,40 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {Button, View, Text, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import axios from 'axios';
-import {defaultStyles, LINK, linkerURI} from "../../styles";
+import {defaultStyles,linkerURI} from "../../styles";
 import CustomButton from "../../CustomButton";
 import UploadFile from "./UploadFile";
-import {pusher, requesterFunc} from "../../bll";
+import {pusher} from "../../bll";
 
 const UploadsComp = ({route}) => {
     const { token } = route.params;
     const [fileUri, setFileUri] = useState(null);
-    // const da = requesterFunc('GET', linkerURI.myfiles, { headers: { Authorization: `Token ${token}` } }).data
-    // console.log(da)
-
-    // const da = [
-    //     {
-    //         "slug": "5b769e9f-b4d6-4fa1-a664-1e54970b6562",
-    //         "createdAt": "2023-05-15T21:54:52.143472+03:00",
-    //         "downloded": 0,
-    //         "seen": -1,
-    //         "file": "http://26.242.229.65:8000/5b769e9f-b4d6-4fa1-a664-1e54970b6562.rar",
-    //         "name": "Файлы для Mac OC.rar",
-    //         "content_type": "application/octet-stream",
-    //         "access": false
-    //     },
-    //     {
-    //         "slug": "5b769e9f-b4d6-4fa1-a664-1e54970b6562",
-    //         "createdAt": "2023-05-15T21:54:52.143472+03:00",
-    //         "downloded": 0,
-    //         "seen": -1,
-    //         "file": "http://26.242.229.65:8000/5b769e9f-b4d6-4fa1-a664-1e54970b6562.rar",
-    //         "name": "Файлы для Mac OC.rar",
-    //         "content_type": "application/octet-stream",
-    //         "access": true
-    //     }
-    // ]
 
     const handleFilePick = async () => {
         // { type: 'application/zip' }
@@ -43,6 +18,20 @@ const UploadsComp = ({route}) => {
             setFileUri(result.uri);
         }
     };
+
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const getFiles = async () => {
+            console.log(token)
+            const res = await axios.get(linkerURI.myfiles, { headers: { Authorization: `Token ${token}` } })
+
+            setData(res.data)
+            console.log(data)
+            // console.log('data')
+        }
+        getFiles()
+    }, [])
 
     const handleFileUpload = async () => {
         let fileType = fileUri.split('.').pop();
@@ -64,24 +53,12 @@ const UploadsComp = ({route}) => {
         } catch (error) {
             console.log(error);
         }
-        setFileUri(null)
-    };
-    const [data, setData] = useState([])
-    useEffect(() => {
-        const getFiles = async () => {
-            console.log(token)
-            const res = await axios.get(linkerURI.myfiles, { headers: { Authorization: `Token ${token}` } })
+        setFileUri(null);
 
-            setData(res.data)
-            console.log(data)
-            // console.log('data')
-        }
-        getFiles()
-    }, [])
+    };
 
     return (
         <View style={css.bg}>
-            {/*<Text>Token: {token}</Text>*/}
             {fileUri ? (
                 <Text>Вы выбрали: {fileUri}</Text>
             ) : (
@@ -89,7 +66,6 @@ const UploadsComp = ({route}) => {
                     <Text style={css.uploadBtn__text}>+</Text>
                 </TouchableOpacity>
             )}
-            {/*<Button title="Upload" onPress={handleFileUpload} disabled={!fileUri} />*/}
             <CustomButton
                 text={'Upload'}
                 actionFunc={handleFileUpload}
@@ -116,7 +92,6 @@ const css = StyleSheet.create({
         paddingVertical: 8,
         marginVertical: 8,
         aspectRatio: '1/1',
-        // width: '40%',
         borderRadius: 100,
 
         display: 'flex',
